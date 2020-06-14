c1ed55c7b70a40e161057182a21ce1735642b28a
Blexn <Onyeka_blessing@yahoo.com>
2020-06-14 15:06:19 +0100

    Initial commit

---
+++ app/src/main/java/com/blexn/gamefragment/StartFragment.kt
@@ -0,0 +1,28 @@
+package com.blexn.gamefragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class StartFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+}
